0bf2f9f5c5a3e6edc2490d70b03a3e7b78296c14
rim <sandrochia@naver.com>
2020-06-04 16:01:38 +0900

    17강. JPA 데이터 Common 1. Repository

--- demospringdata/src/main/java/com/metamong/demospringdata/PostRepository.java
+++ demospringdata/src/main/java/com/metamong/demospringdata/PostRepository.java
@@ -1,8 +1,13 @@
 package com.metamong.demospringdata;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    Page<Post> findByTitleContains(String title, Pageable pageable);
+
+    long countByTitleContains(String title);
 }
