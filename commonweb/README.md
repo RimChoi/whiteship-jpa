a9b6aecd9ee9a625e40385dc9dd91dfa5212eea0
rim <sandrochia@naver.com>
2020-06-05 16:01:26 +0900

    33강. 스프링 데이터 JPA 1. JpaRepository

--- commonweb/README.md
+++ commonweb/README.md
@@ -6,3 +6,5 @@
 > 29강. 스프링 데이터 Common 12. 웹기능 2부 DomainClassConverter<br>
 > 30강. 스프링 데이터 Common 13. 웹기능 3부 Pageable과 Sort<br>
 > 31강. 스프링 데이터 Common 14. 웹기능 4부 Hateoas<br>
+> 32강. 스프링 데이터 Common 15. 정리<br>
+> 33강. 스프링 데이터 JPA 1. JpaRepository<br>
---
+++ commonweb/src/test/java/com/metamong/commonweb/post/PostRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.metamong.commonweb.post;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class PostRepositoryTest {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Test
+    public void crud() {
+        Post post = new Post();
+        post.setTitle("jpa");
+        postRepository.save(post);
+
+        List<Post> posts = postRepository.findAll();
+        assertThat(posts.size()).isEqualTo(1);
+    }
+
+}
