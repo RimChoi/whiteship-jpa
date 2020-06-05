97f85e8f8fc86f9040a803b0402f717f15293d5a
rim <sandrochia@naver.com>
2020-06-05 14:59:29 +0900

    30강. 스프링 데이터 Common 13. 웹기능 3부 Pageable과 Sort

--- commonweb/README.md
+++ commonweb/README.md
@@ -4,3 +4,4 @@
 ### 해당 강좌
 
 > 29강. 스프링 데이터 Common 12. 웹기능 2부 DomainClassConverter<br>
+> 30강. 스프링 데이터 Common 13. 웹기능 3부 Pageable과 Sort<br>
--- commonweb/src/main/java/com/metamong/commonweb/post/PostController.java
+++ commonweb/src/main/java/com/metamong/commonweb/post/PostController.java
@@ -1,6 +1,8 @@
 package com.metamong.commonweb.post;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +13,19 @@ import java.util.Optional;
 public class PostController {
 
     @Autowired
-    PostRepository postRepository;
+    PostRepository posts;
 
     @GetMapping("/posts/{id}")
-    public String getPost(@PathVariable("id") Post post) {
+    public String getPosts(@PathVariable("id") Post post) {
         // DomainClassConverter 에서 아래 과정을 해준다 ..!
 //        Optional<Post> byId = postRepository.findById(id);
 //        Post post = byId.get();
 
         return post.getTitle();
+    }
+
+    @GetMapping("/posts")
+    public Page<Post> getPosts(Pageable pageable) {
+        return posts.findAll(pageable);
     }
 }
